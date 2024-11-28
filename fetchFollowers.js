eb07cc05dc746a7c15b9970488bf716ff0c850bc
Achi Destiny <achinzube@gmail.com>
2024-11-28 11:47:43 +0100

    first commit

---
+++ fetchFollowers.js
@@ -0,0 +1,11 @@
+const url = "https://api.github.com/users/john-smilga/followers?per_page=100";
+
+const fetchFollowers = async () => {
+  try {
+    const resp = await fetch(url);
+    const data = await resp.json();
+    return data;
+  } catch (error) {}
+};
+
+export default fetchFollowers;
